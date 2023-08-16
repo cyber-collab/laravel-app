e752e2a1e673672960ed8278eb76ba3d0b64ccf2
Den <den.kramar.stratis@gmail.com>
2023-08-16 09:49:49 +0300

    [TASK] Add Employee

---
+++ database/migrations/2023_08_16_064009_create_employees_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    // database/migrations/xxxx_xx_xx_create_employees_table.php
+
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('position');
+            $table->date('hire_date');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->decimal('salary', 10, 2);
+            $table->string('photo')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('admin_created_id')->nullable();
+            $table->unsignedBigInteger('admin_updated_id')->nullable();
+
+            $table->foreign('admin_created_id')->references('id')->on('users');
+            $table->foreign('admin_updated_id')->references('id')->on('users');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
